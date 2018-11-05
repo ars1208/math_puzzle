9693b115034d0030ce3509ff649bdd9155881721
ars1208 <ara.kaz1208@gmail.com>
2018-11-05 23:52:22 +0900

    code add

---
+++ q01_1.rb
@@ -0,0 +1,12 @@
+N = 4
+
+cnt = 0
+0.upto(N) do |rock|
+    0.upto(N - rock) do |scissors|
+        paper = N - rock -scissors
+        all = [rock, scissors, paper]
+        cnt += 1 if all.count(all.max) == 1
+    end
+end
+
+puts cnt
