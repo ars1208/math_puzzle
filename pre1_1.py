c8b77246deab5690ae3ca75bed0a6984f59c9c0b
ars1208 <ara.kaz1208@gmail.com>
2020-02-10 15:48:35 +0900

    python ver update

--- pre1_1.py
+++ pre1_1.py
@@ -1,15 +1,15 @@
 m, n = 10, 100
 
-def check(remain, pre):
-    # 配置する人がいなくなったら終了
-    if remain < 0:
-        return 0
-    if remain == 0:
+def check(x, t): # 再帰的処理を行なうために関数化。xが残っている人数で、tが前のテーブルの人数
+
+    if x == 0:
         return 1
+    elif x < 0:
+        return 0
 
-    count = 0
-    for i in range(pre,m+1):
-        count += check(remain -i, i)
-    return count
+    cnt = 0
+    for i in range(t,m+1): # テーブルに座る人数を求める
+        cnt += check(x-i, i)
+    return cnt
 
-print(check(n, 2))
+print(check(n,2))
